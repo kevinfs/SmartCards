1a9c22942dc8634b5c0af8beeddfca3bc0e9be8e
taberkab <taberkanebelkacem@gmail.com>
2017-01-04 22:51:47 +0100

    md5 and alea methods

---
+++ cryptoCard/src/main/java/com/iris/controller/Tools.java
@@ -0,0 +1,35 @@
+package tools;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Tools {
+
+	@SuppressWarnings("unused")
+	private String md5(String password) {
+		byte[] uniqueKey = password.getBytes();
+		byte[] hash = null;
+
+		try {
+			hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
+		} catch (NoSuchAlgorithmException e) {
+			throw new Error("No MD5 support in this VM.");
+		}
+
+		StringBuilder hashString = new StringBuilder();
+		for (int i = 0; i < hash.length; i++) {
+			String hex = Integer.toHexString(hash[i]);
+			if (hex.length() == 1) {
+				hashString.append('0');
+				hashString.append(hex.charAt(hex.length() - 1));
+			} else
+				hashString.append(hex.substring(hex.length() - 2));
+		}
+		return hashString.toString();
+	}
+
+	public static int alea(int n) {
+		return (int) (Math.random() * n);
+	}
+
+}
