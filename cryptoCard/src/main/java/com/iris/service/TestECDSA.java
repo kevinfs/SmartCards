0bff55a5f227e2801b0a3bc85b9b4cd4428f810b
kevinfs <kevinfs@me.com>
2017-01-12 16:09:41 +0100

    Cleanup + static on verifECDSA

--- cryptoCard/src/main/java/com/iris/service/TestECDSA.java
+++ cryptoCard/src/main/java/com/iris/service/TestECDSA.java
@@ -1,18 +1,16 @@
 package com.iris.service;
 
 public class TestECDSA {
-	
-	public static void main(String [] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 
 		ECDSA e = new ECDSA("SmartCards");
 		ECDSA e2 = new ECDSA("CarteAPuce");
-		
-		Tools t = new Tools();
-		
-		boolean b = t.verifECDSA(e.getBaText(), e.getPubKey(), e.getBaSignature());
+
+		boolean b = Tools.verifECDSA(e.getBaText(), e.getPubKey(), e.getBaSignature());
 		System.out.println("Valid : " + b + "\n");
-		
-		boolean b2 = t.verifECDSA(e.getBaText(), e2.getPubKey(), e.getBaSignature());
+
+		boolean b2 = Tools.verifECDSA(e.getBaText(), e2.getPubKey(), e.getBaSignature());
 		System.out.println("Valid : " + b2);
 	}
 }
