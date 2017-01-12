b6d1812dbe28e0492b815f8253446ce8181d57c7
kevinfs <kevinfs@me.com>
2017-01-12 09:23:58 +0100

    Resolved conflicts

--- cryptoCard/src/main/java/com/client/controller/HttpRequestBuilderTest.java
+++ cryptoCard/src/main/java/com/client/controller/HttpRequestBuilderTest.java
@@ -1,50 +1,48 @@
 package com.client.controller;
 
-import com.iris.service.Tools;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.iris.service.Tools;
 
 public class HttpRequestBuilderTest {
 
+	public static void main(String[] args) throws Exception {
+		Tools tools = new Tools();
+		String url;
+		MultiValueMap<String, String> requestData;
 
-    public static void main(String[] args) throws Exception {
-        Tools tools = new Tools();
-        String url;
-        MultiValueMap<String, String> requestData;
+		// params
+		final String login = "caca";
+		final String password = "caca";
 
-        //params
-        final String login = "caca";
-        final String password = "caca";
+		// initialisation
+		String baseURL = "https://localhost:8088";
+		HttpRequestBuilder http = new HttpRequestBuilder(baseURL);
 
-//        initialisation
-        String baseURL = "https://localhost:8088";
-        HttpRequestBuilder http = new HttpRequestBuilder(baseURL);
+		// récupération graine
+		url = "https://localhost:8088/graine";
+		String graine = http.get(url);
+		System.out.println("graine = " + graine);
 
-//        récupération graine
-        url = "https://localhost:8088/graine";
-        String graine = http.get(url);
-        System.out.println("graine = " + graine);
+		// envoi login
+		url = "https://localhost:8088/login";
+		requestData = new LinkedMultiValueMap<>();
+		requestData.add("login", login);
+		String sel = http.post(url, requestData);
+		System.out.println("sel = " + sel);
 
-//        envoi login
-        url = "https://localhost:8088/login";
-        requestData = new LinkedMultiValueMap<>();
-        requestData.add("login", login);
-        String sel = http.post(url, requestData);
-        System.out.println("sel = " + sel);
+		// envoi mdp
+		url = "https://localhost:8088/password";
 
-//        envoi mdp
-        url = "https://localhost:8088/password";
+		requestData = new LinkedMultiValueMap<>();
+		String v1 = tools.md5(password + sel);
+		String v2 = tools.md5(v1 + graine);
+		requestData.add("login", login);
+		requestData.add("password", v2);
+		requestData.add("graine", graine);
+		String response = http.post(url, requestData);
 
-        requestData = new LinkedMultiValueMap<>();
-        String v1 = tools.md5(password + sel);
-        String v2 = tools.md5(v1 + graine);
-        requestData.add("login", login);
-        requestData.add("password", v2);
-        requestData.add("graine", graine);
-        String response = http.post(url, requestData);
-
-        System.out.println("response = " + response);
-    }
+		System.out.println("response = " + response);
+	}
 }
