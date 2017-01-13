37aa592ddce37e317fe260c41b2969ea9d979397
kevinfs <kevinfs@me.com>
2017-01-13 11:29:05 +0100

    New fields in User

--- cryptoCard/src/main/java/com/iris/model/User.java
+++ cryptoCard/src/main/java/com/iris/model/User.java
@@ -1,6 +1,5 @@
 package com.iris.model;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,43 +8,61 @@ import javax.persistence.Id;
 @Entity
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String login;
-    private String password;
-    private String sel;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String login;
+	private String password;
+	private String sel;
+	private String publicKey;
+	private Long numberToSign;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String getSel() {
-        return sel;
-    }
+	public String getSel() {
+		return sel;
+	}
 
-    public void setSel(String sel) {
-        this.sel = sel;
-    }
+	public void setSel(String sel) {
+		this.sel = sel;
+	}
+
+	public String getPublicKey() {
+		return publicKey;
+	}
+
+	public void setPublicKey(String publicKey) {
+		this.publicKey = publicKey;
+	}
+
+	public Long getNumberToSign() {
+		return numberToSign;
+	}
+
+	public void setNumberToSign(Long numberToSign) {
+		this.numberToSign = numberToSign;
+	}
+
 }
-
